27895e8eb9445136dbbee243f49f7e3ebaf023c7
Bleron Limani <bleroni@gmail.com>
2024-03-16 10:45:35 +0100

    Iterating on automatic uplod process

--- functions.py
+++ functions.py
@@ -1,3 +1,56 @@
+import shutil
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_payload(sentence):
+  payload = {
+        "domain": "general",
+        "localeId": 71,
+        "localeName": "sq",
+        "sentence": sentence,
+        "source": 'KC'  
+    }
+  return payload
+
+def get_headers():
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Content-Length': '165',  # This is usually calculated automatically
+        'Content-Type': 'application/json; charset=utf-8',
+        'Cookie': os.environ.get('MOZILLA_COOKIE'),
+        'Origin': 'https://commonvoice.mozilla.org',
+        'Referer': 'https://commonvoice.mozilla.org/sq/write',
+        'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"macOS"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0 Safari/537.36',
+    }
+
+    return headers
+
+def copy_file(source_path, destination_path):
+    """
+    Copies a file from source_path to destination_path.
+    
+    :param source_path: The path to the file to be copied.
+    :param destination_path: The path where the file should be copied to.
+    """
+    # Ensure the destination directory exists, if not, create it.
+    if not os.path.exists(os.path.dirname(destination_path)):
+        os.makedirs(os.path.dirname(destination_path))
+    
+    # Copy the file to the new location
+    shutil.copy(source_path, destination_path)
+    print(f"File {source_path} has been copied to {destination_path}")
+    return destination_path
+
 def fix_split_word(input_text, output_text):
     pairs = [
         (" aditur", "paditur"),
--- upload.py
+++ upload.py
@@ -1,65 +1,61 @@
 import requests
 import time
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from functions import copy_file, get_headers, get_payload
 
-
-file1 = open('clean_files/copa_2_bleron.txt', 'r')
-lines = file1.readlines()
-
-
-
-# Define the URL and API endpoint
-url = 'https://commonvoice.mozilla.org/api/v1/sentences'
-
-
-# Define the headers as shown in the screenshot
-headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Content-Length': '165',  # This is usually calculated automatically
-    'Content-Type': 'application/json; charset=utf-8',
-    'Cookie': os.environ.get('MOZILLA_COOKIE'),
-    'Origin': 'https://commonvoice.mozilla.org',
-    'Referer': 'https://commonvoice.mozilla.org/sq/write',
-    'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'Sec-Ch-Ua-Platform': '"macOS"',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0 Safari/537.36',
-}
-
-# The payload would be included if you had data to send with the POST request
-
-
-# Perform the POST request
-
-count = 0
-# Strips the newline character
-for line in lines:
-    count += 1
-    print("Line{}: {}".format(count, line.strip()))
-    payload = {
-        "domain": "general",
-        "localeId": 71,
-        "localeName": "sq",
-        "sentence": line,
-        "source": 'KC'  
-    }
-    print(payload)
+def post_request_to_mozilla(sentence):
+    # Define the URL and API endpoint
+    url = 'https://commonvoice.mozilla.org/api/v1/sentences'
+    headers = get_headers()
+    payload = get_payload(sentence)
     response = requests.post(url, headers=headers, json=payload)
-
-
-
-    # Check the response
     if response.ok:
         print('Response successful:', response.text)
+        return 'success'
     else:
-        print(str(count) + ': XXXXXXXX Request failed in text: ' + line)
+        print(str(count) + ': XXXXXXXX Request failed in text: ' + sentence)
         print('Status code:', response.status_code)
+        json_resp = response.json()
+        print(json_resp)
+        
+        if json_resp.get('errorType') is not None:
+            return json_resp['errorType']
+        else:
+            return 'APPLICATION_500_ERROR'    
 
-    time.sleep(3)    
+def save_list_to_file(list_of_strings, file_path):
+    """
+    Saves a list of strings to a file, with each string on a new line.
+    
+    :param list_of_strings: List of strings to be saved.
+    :param file_path: The path of the file where the list will be saved.
+    """
+    with open(file_path, 'w') as file:
+        for item in list_of_strings:
+            file.write(f"{item}\n")
+
+# new_filename = copy_file('clean_files/copa_2_bleron.txt', 'temp_folder/copa_2_bleron.txt')
+new_filename = 'temp_folder/copa_2_bleron.txt'
+sentences = open(new_filename, 'r').readlines()
+
+# sentences = ['Kjo fjale eshte me iniciale I.L. dhe R.A. nder te tjera', 'Kjo fjali është shumë e thjeshtë.', 
+#              'Prape fjali me iniciale I.L. dhe R.A., e cila nuk do duhej funksionuar', 'Tirana dhe Prishtina janë qytete shumë të bukura']
+
+failed_requests = []
+successful_requests = []
+
+count = 0
+while sentences:
+    count += 1
+    sentence = sentences.pop(0)
+    print(f"Processing sentence {count}: {sentence}")
+    resp = post_request_to_mozilla(sentence)
+    if resp == 'success':
+      successful_requests.append(sentence)
+      save_list_to_file(successful_requests, 'temp_folder/successful_requests.txt')
+    else: 
+      # failed_requests.append(sentence)
+      # save_list_to_file(failed_requests, f'temp_folder/failed_requests/{resp}.txt')
+      with open(f'temp_folder/failed_requests/{resp}.txt', 'a') as file1:
+        file1.write(sentence)
+
+    time.sleep(2)    
