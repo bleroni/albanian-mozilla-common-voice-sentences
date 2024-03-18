8e9c6e91900b40b9918a2870f3e7bf6c1dd96b60
Bleron Limani <bleroni@gmail.com>
2024-03-18 10:44:28 +0100

    Decoupling functions

--- upload.py
+++ upload.py
@@ -1,45 +1,11 @@
 import requests
 import time
-from functions import copy_file, get_headers, get_payload
+from functions import copy_file, post_request_to_mozilla
 
-def post_request_to_mozilla(sentence):
-    # Define the URL and API endpoint
-    url = 'https://commonvoice.mozilla.org/api/v1/sentences'
-    headers = get_headers()
-    payload = get_payload(sentence)
-    response = requests.post(url, headers=headers, json=payload)
-    if response.ok:
-        print('Response successful:', response.text)
-        return 'success'
-    else:
-        print(str(count) + ': XXXXXXXX Request failed in text: ' + sentence)
-        print('Status code:', response.status_code)
-        json_resp = response.json()
-        print(json_resp)
-        
-        if json_resp.get('errorType') is not None:
-            return json_resp['errorType']
-        else:
-            return 'APPLICATION_500_ERROR'    
-
-def save_list_to_file(list_of_strings, file_path):
-    """
-    Saves a list of strings to a file, with each string on a new line.
-    
-    :param list_of_strings: List of strings to be saved.
-    :param file_path: The path of the file where the list will be saved.
-    """
-    with open(file_path, 'w') as file:
-        for item in list_of_strings:
-            file.write(f"{item}\n")
-
-target_file = 'copa_4_kushtrim.txt'
-# new_filename = copy_file(f'clean_files/{target_file}', f'temp_folder/{target_file}')
-new_filename = 'temp_folder/copa_4_kushtrim.txt'
+target_file = 'copa_8_bleron_editing'
+new_filename = copy_file(f'temp_folder/{target_file}.txt', f'temp_folder/{target_file}_uploading.txt')
+# new_filename = 'temp_folder/copa_4_kushtrim.txt'
 sentences = open(new_filename, 'r').readlines()
-
-# sentences = ['Kjo fjale eshte me iniciale I.L. dhe R.A. nder te tjera', 'Kjo fjali është shumë e thjeshtë.', 
-#              'Prape fjali me iniciale I.L. dhe R.A., e cila nuk do duhej funksionuar', 'Tirana dhe Prishtina janë qytete shumë të bukura']
 
 failed_requests = []
 successful_requests = []
