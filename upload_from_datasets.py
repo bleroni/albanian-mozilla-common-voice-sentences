073dcbcea57a79373963a5257031380ecda882a8
Bleron Limani <bleroni@gmail.com>
2024-03-31 21:25:27 +0200

    0 to 3000 tenders processed

--- upload_from_datasets.py
+++ upload_from_datasets.py
@@ -4,15 +4,16 @@ from functions import copy_file, post_request_to_mozilla, fix_words, get_word_le
 dataset = load_dataset("bleroni/kosovo-government-contract-titles", split="train")
 list_dataset = dataset.to_list()
 
-for count, row in enumerate(list_dataset[205:1205]):
+for count, row in enumerate(list_dataset[2000:3000]):
   sentence = fix_words(row['Title'])  
   
   word_length = get_word_length(sentence)
   if word_length < 3 or word_length > 15:
     continue
 
+  print(count, sentence)
   resp = post_request_to_mozilla(sentence, count)
-  # print(count, sentence)
+  
   if resp != 'success':
       with open(f'temp_folder/datasets/failed_requests/{resp}.txt', 'a') as file1:
         file1.write(sentence)
